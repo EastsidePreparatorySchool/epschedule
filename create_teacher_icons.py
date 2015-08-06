7b04d707384facde1cfd9fcb352c97ce43a67d84
guberti <gavin.uberti@gmail.com>
2015-08-05 20:53:25 -0700

    Add Image Cropping Exceptions
    - Added a list of exceptions to create_teacher_icons.py so that for
    photos where the teacher isn't centered, the program won't crop out
    their head
    - Generated a crypto key to encrypt cookies with (not synced, for
    obvious reasons)

--- create_teacher_icons.py
+++ create_teacher_icons.py
@@ -1,16 +1,25 @@
 from wand.image import Image
 import os
 
+exceptions = [["hayes.jpg", 20, 0], ["hollingshead.jpg", 20, 0], ["pedersen.jpg", -20, 0], ["sweet.jpg", 40, 0]]
 for image in os.listdir("teacher_photos"):
     with Image(filename=('teacher_photos\\' + str(image))) as img:
         if (img.width > img.height):
             img.resize((96 * img.width) / img.height, 96)
             border = (img.width - 96) / 2
-            img.crop(border, 0, width=96, height=96)
+            cropparams = [border, 0]
         else:
             img.resize(96, (96 * img.height) / img.width)
             border = (img.height - 96) / 2
-            img.crop(0, border, width=96, height=96)
+            cropparams = [0, border]
+            
+        for exception in exceptions:
+            if str(image) == exception[0]:
+                print "Exception excpetion"
+                cropparams[0] += exception[1]
+                cropparams[1] += exception[2]
+                print cropparams
+        img.crop(cropparams[0], cropparams[1], width=96, height=96)
         img.convert('jpeg')
         
         img.save(filename='96x96_photos\\' + os.path.splitext(image)[0] + '.jpg')
