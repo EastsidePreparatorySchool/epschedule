52ad65314b5159cc1d3a6011af9c902d11b76f6b
guberti <gavin.uberti@gmail.com>
2015-09-12 22:00:35 -0700

    Fixed comment in PR

--- main.py
+++ main.py
@@ -138,13 +138,15 @@ class BaseHandler(webapp2.RequestHandler): # All handlers inherit from this hand
         return True
 
     def get_schedule_for_name(self, firstname, lastname):
-        for schedule in SCHEDULE_INFO:
+        schedule_data = get_schedule_data()
+        for schedule in schedule_data:
             if schedule['firstname'].lower() == firstname.lower() and \
                schedule['lastname'].lower() == lastname.lower(): # If the schedule is the requested schedule
                 return schedule.copy()
 
     def get_schedule_for_id(self, id):
-        for schedule in SCHEDULE_INFO:
+        schedule_data = get_schedule_data()
+        for schedule in schedule_data:
             if schedule["id"] == str(id): # If the schedule is the user's schedule
                 return schedule.copy()
 
