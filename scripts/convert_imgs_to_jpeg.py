b092e637dc0ea59b7d59ee9283374a35ef7b62eb
guberti <gavin.uberti@gmail.com>
2015-09-15 21:44:06 -0700

    Fixed spelling errors

--- scripts/convert_imgs_to_jpeg.py
+++ scripts/convert_imgs_to_jpeg.py
@@ -15,7 +15,7 @@ def find_shift(name, axis):
 				return exception[2]
 	return 0
 
-def apply_excetion(name, number, axis, axisvalue): # Takes in the name, current left or top value, "x" or "y", and the length of the x or y of the image
+def apply_exception(name, number, axis, axisvalue): # Takes in the name, current left or top value, "x" or "y", and the length of the x or y of the image
 	shift = find_shift(name, axis)
 	if shift == 0:
 		return number
@@ -32,10 +32,10 @@ for image in os.listdir("../teacher_photos"):
     with Image(filename=('../teacher_photos/' + str(image))) as img:
         if img.width > img.height:
             left = (img.width - img.height) / 2
-            left = apply_excetion(str(image), left, "x", img.width)
+            left = apply_exception(str(image), left, "x", img.width)
             img.crop(left, 0, width=img.height, height=img.height)
         elif img.width < img.height:
             top = (img.height - img.width) / 2
-            top = apply_excetion(str(image), top, "y", img.height)
+            top = apply_exception(str(image), top, "y", img.height)
             img.crop(0, top, width=img.width, height=img.width)
         img.save(filename='..\\teacher_photos_fullsize\\' + os.path.splitext(image)[0] + '.jpg')
