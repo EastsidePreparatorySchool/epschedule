58d468f1c962ef84586ec653d7cf07e24967e62d
guberti <gavin.uberti@gmail.com>
2015-09-15 21:38:35 -0700

    Update exceptions to work properly

--- scripts/convert_imgs_to_jpeg.py
+++ scripts/convert_imgs_to_jpeg.py
@@ -1,12 +1,41 @@
 from wand.image import Image
 import os
+import json
+
+exceptions_file = open('photo_exceptions.json', 'rb')
+exceptions = json.load(exceptions_file)
+print exceptions
+
+def find_shift(name, axis):
+	for exception in exceptions:
+		if exception[0][:-4] == name[:-4]:
+			if axis == "x":
+				return exception[1]
+			elif axis == "y":
+				return exception[2]
+	return 0
+
+def apply_excetion(name, number, axis, axisvalue): # Takes in the name, current left or top value, "x" or "y", and the length of the x or y of the image
+	shift = find_shift(name, axis)
+	if shift == 0:
+		return number
+
+	shift *= (axisvalue / 96)
+	shift = int(round(shift)) # Round shift to the nearest int
+	number += shift
+
+	return number
+
+
 
 for image in os.listdir("../teacher_photos"):
     with Image(filename=('../teacher_photos/' + str(image))) as img:
         if img.width > img.height:
             left = (img.width - img.height) / 2
+            left = apply_excetion(str(image), left, "x", img.width)
             img.crop(left, 0, width=img.height, height=img.height)
         elif img.width < img.height:
             top = (img.height - img.width) / 2
+            top = apply_excetion(str(image), top, "y", img.height)
             img.crop(0, top, width=img.width, height=img.width)
         img.save(filename='..\\teacher_photos_fullsize\\' + os.path.splitext(image)[0] + '.jpg')
--- scripts/create_teacher_icons.py
+++ scripts/create_teacher_icons.py
@@ -1,7 +1,10 @@
 from wand.image import Image
 import os
+import json
 
-exceptions = [["hayes.jpg", 17, 0], ["hollingshead.jpg", 20, 0], ["pedersen.jpg", -20, 0], ["sweet.jpg", 24 , 0]]
+exceptions_file = open('photo_exceptions.json', 'rb')
+exceptions = json.load(exceptions_file)
+
 for image in os.listdir("../teacher_photos"):
     print "Cropping " + str(image)
     with Image(filename=('..\\teacher_photos\\' + str(image))) as img:
@@ -20,7 +23,6 @@ for image in os.listdir("../teacher_photos"):
             if str(image) == exception[0]:
                 x_except += exception[1]
                 y_except += exception[2]
-        print "Getting here!"
         print "Calling: img.crop(" + str(cropparams[0] + x_except) + ", " + str(cropparams[1] + y_except) + ", " + str(img.width - cropparams[0] + x_except) + ", " + str(img.height - cropparams[1] + y_except) + ")"
         img.crop(cropparams[0] + x_except, cropparams[1] + y_except, img.width - cropparams[0] + x_except, img.height - cropparams[1] + y_except)
         img.convert('jpeg')
