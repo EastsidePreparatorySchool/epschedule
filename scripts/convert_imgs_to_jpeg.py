86b6a4aceb76ac67b467f60cf89e617043798266
guberti <gavin.uberti@gmail.com>
2015-09-15 20:59:51 -0700

    Fix indentation
    Used tabs before, now using spaces

--- scripts/convert_imgs_to_jpeg.py
+++ scripts/convert_imgs_to_jpeg.py
@@ -3,10 +3,10 @@ import os
 
 for image in os.listdir("../teacher_photos"):
     with Image(filename=('../teacher_photos/' + str(image))) as img:
-    	if img.width > img.height:
-    		left = (img.width - img.height) / 2
-    		img.crop(left, 0, width=img.height, height=img.height)
-       	elif img.width < img.height:
-    		top = (img.height - img.width) / 2
-    		img.crop(0, top, width=img.width, height=img.width)
+        if img.width > img.height:
+            left = (img.width - img.height) / 2
+            img.crop(left, 0, width=img.height, height=img.height)
+        elif img.width < img.height:
+            top = (img.height - img.width) / 2
+            img.crop(0, top, width=img.width, height=img.width)
         img.save(filename='..\\teacher_photos_fullsize\\' + os.path.splitext(image)[0] + '.jpg')
