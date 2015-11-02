eb0b39b9001aa3af1a7da0fcfb114bba1dfd0af6
guberti <gavin.uberti@gmail.com>
2015-11-02 08:41:44 -0800

    Add in lunch parsing file
    Still need to add in code to write to a DB and write code to install
    icalendar on the server
    Plus need to set up the cron task to run each minute

--- cron/update_lunch.py
+++ cron/update_lunch.py
@@ -5,6 +5,35 @@ import cgi
 import string
 import os
 import sys
-from google.appengine.ext import db
+from icalendar import Calendar, Event
+import unicodedata
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    return only_ascii
+
+lunches = []
 
 url = "http://www.eastsideprep.org/?plugin=all-in-one-event-calendar&controller=ai1ec_exporter_controller&action=export_events&ai1ec_cat_ids=57?"
+req = urllib2.Request(url)
+response = urllib2.urlopen(req)
+data = response.read()
+
+cal = Calendar.from_ical(data)
+
+for event in cal.walk('vevent'):
+
+    date = event.get('dtstart').dt
+    summary = event.get('summary')
+    description = remove_accents(event.get('description'))
+
+    obj = { \
+        "summary": str(summary), \
+        "description": str(description), \
+        "day": date.day, \
+        "month": date.month, \
+        "year": date.year}
+    lunches.append(obj)
+
+print lunches
