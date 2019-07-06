3c655cc234ecc94f6f70f518098b17682f633730
Gavin Uberti <gavin.uberti@gmail.com>
2019-07-06 12:37:56 -0700

    Remove unused scripts

--- scripts/remove_extra_pdf_extensions.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path = "C:/Users/guberti/Documents/Github/EPSchedule/schedules"
-
-for fileName in os.listdir(path):
-    if (fileName[-8:] == ".pdf.pdf"):
-        os.rename(path + "/" + fileName, path + "/" + fileName[:-4])
