20a9475c84bf0dd4d8fda75f72049def8960ca24
Justin Uberti <justin@uberti.name>
2021-02-22 21:05:18 -0800

    Use with to open/close file cleanly

--- main_test.py
+++ main_test.py
@@ -7,7 +7,8 @@ from PIL import Image
 
 from app import app, init_app
 
-TEST_SCHEDULES = json.load(open('data/test_schedule.json'))
+with open('data/test_schedule.json', 'r') as f:
+    TEST_SCHEDULES = json.load(f)
 
 TEST_MASTER_SCHEDULE = [{
     "2020-09-09": "Remote A-D_Rem",
