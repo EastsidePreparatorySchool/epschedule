3363e0c0ae726a54cb76ae299453ae974893f168
kuberti <31712156+kuberti@users.noreply.github.com>
2021-09-18 13:44:10 -0700

    Stylistic changes

--- scripts/convert_usernames.py
+++ scripts/convert_usernames.py
@@ -1,20 +1,20 @@
 import csv
 import json
 
-def make_json(csv_file_path, json_file_path):
+def make_json(CSV_FILE_PATH, JSON_FILE_PATH):
     data = []
 
     with open(csv_file_path, encoding='utf=8') as csvf:
         csv_reader = csv.DictReader(csvf)
 
-        for row in csv_reader:
-            data.append(row['login'])
+    for row in csv_reader:
+        data.append(row['login'])
 
     with open(json_file_path, 'w', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent=4))
 
 
-csv_file_path = 'four11_map.csv'
-json_file_path = 'four11_map.json'
+CSV_FILE_PATH = 'four11_map.csv'
+JSON_FILE_PATH = 'four11_map.json'
 
-make_json(csv_file_path, json_file_path)
+make_json(CSV_FILE_PATH, JSON_FILE_PATH)
