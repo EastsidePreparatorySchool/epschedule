1438548dc70c2b0d2f8ab0d4a5b0db62d4a8ae9e
juberti <justin@fixie.ai>
2023-01-19 14:40:38 -0800

    Fix tests

--- cron/test_lunch.py
+++ cron/test_lunch.py
@@ -6,15 +6,14 @@ from cron.update_lunch import add_events
 
 # Test reading lunch data from an ICS file and sanitizing it.
 def test_read_lunches():
-    events = []
-    with patch(
-        "cron.update_lunch.write_event_to_db", side_effect=lambda c, e: events.append(e)
-    ) as mock:
+    patch("google.cloud.ndb.Client")
+    with patch("cron.update_lunch.write_event_to_db") as mock:
         ICS_PATH = "data/test_lunch.ics"
         # Reads the ICS file to get a lunch object for 10/7/2019, Cheese Manicotti
         fake_response = open(ICS_PATH).read()
         add_events(fake_response)
-        assert len(events) == 1
-        event = events[0]
+
+        assert mock.call_count == 1  # summary() == "Cheese Manicotti"
+        event = mock.call_args[0][1]
         assert event.summary == "Monday | Cheese Manicotti"
         assert event.day == datetime.date(2019, 10, 7)
