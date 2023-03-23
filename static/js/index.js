4c6a3576859590d87f5181763496dcd4d1e8ca70
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-03-23 11:14:54 -0700

    WIP transition based on system scheme

--- static/js/index.js
+++ static/js/index.js
@@ -395,7 +395,7 @@ function renderStudent(studentObj) {
     var name = studentObj.firstname + " " + studentObj.lastname;
   }
   popupContainer.innerHTML = '<div class="teacher" layout vertical>' +
-      '<paper-material class="header" elevation="2" style="background-color: #1f1f1f;">' +
+      '<paper-material class="header" elevation="2" style="@media (prefers-color-scheme: light) {background-color:#1f1f1f;} @media (prefers-color-scheme: dark) {background-color:#efefef;}">' +
       '<div layout horizontal center>' +
       '<img src="' + studentObj.photo_url + '" onerror="if (this.src != \'/static/images/placeholder.png\') this.src = \'/static/images/placeholder.png\';">' +
       '<div layout vertical><h3><span class="grade">' + grade + '</span></h3>' +
