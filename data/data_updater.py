aeea17074be93f1c513fb48e372365b5852ed41d
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-08-24 20:08:52 -0700

    Style fixes

--- data/data_updater.py
+++ data/data_updater.py
@@ -4,20 +4,19 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from pandas.tseries.offsets import BDay
 
-f = open('master_schedule.json')
+f = open("master_schedule.json")
 data = json.load(f)
 
-g = open('schedule2.json' ,'w')
+g = open("schedule2.json", "w")
 
 dict = {}
 
 for key in data[0]:
-    date = datetime.strptime(key, '%Y-%m-%d')
-    
+    date = datetime.strptime(key, "%Y-%m-%d")
+
     date = date + relativedelta(years=1)
-    date = date -  BDay(1)
+    date = date - BDay(1)
 
-
-    dict[date.strftime('%Y-%m-%d')] = data[0][key]
+    dict[date.strftime("%Y-%m-%d")] = data[0][key]
 
 g.write(json.dumps(dict))
