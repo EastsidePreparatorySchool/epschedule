6117d827eca6e7287562430dea876c2843b4feda
juberti <justin@fixie.ai>
2024-12-07 11:39:20 -0800

    x

--- cron/test_lunch.py
+++ cron/test_lunch.py
@@ -9,10 +9,16 @@ def test_read_lunches():
     with patch("cron.update_lunch.write_event_to_db") as mock:
         ICS_PATH = "data/test_lunch.ics"
         # Reads the ICS file to get a lunch object for 10/7/2019, Cheese Manicotti
-        fake_response = open(ICS_PATH).read()
+        fake_response = open(ICS_PATH, encoding="utf-8"j).read()
         add_events(fake_response)
 
         assert mock.call_count == 1  # summary() == "Cheese Manicotti"
         event = mock.call_args[0][1]
         assert event.summary == "Monday | Cheese Manicotti"
+        assert event.description == [
+            "Cheese Manicotti with Meat Sauce, Side Salad, and Bread Stick",
+            "Vegetarian: Marinara",
+            "Our lunch today contains the following Allergens:",
+            "Dairy, Gluten, Soy",
+        ]
         assert event.day == datetime.date(2019, 10, 7)
