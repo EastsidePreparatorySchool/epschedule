7d70618c2885270ae60d74fc8230f47fe7072745
Codingses <codingses@outlook.com>
2025-09-25 22:53:20 -0700

    I should have run the runbeforecommit.py file before committing

--- runbeforecommit.py
+++ runbeforecommit.py
@@ -1,4 +1,5 @@
-#Before committing, run this. Make sure you have black installed.
-#Command is "pip install black"
+# Before committing, run this. Make sure you have black installed.
+# Command is "pip install black"
 import os
+
 os.system("python -m black .")
