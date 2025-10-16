924de731788e0135cd68c59b747ac38e4de84849
codingses <149913839+codingses@users.noreply.github.com>
2025-10-15 23:10:08 -0700

    Integrates prettier into runbeforecommit, .gitignore, and tests to add html, js, and css styling (#771)
    * Integrates prettier into runbeforecommit, .gitignore, and tests to ensure proper html, js, and css formatting
    
    * fix dumb mistake
    
    * fix other dumb mistake
    
    * fix last dumb mistake

--- runbeforecommit.py
+++ runbeforecommit.py
@@ -1,9 +1,15 @@
-# Before committing, run this. Make sure you have black, isort, pytest, and autoflake installed.
-# Command is "pip install black isort autoflake pytest"
+# Before committing, run this. Make sure you have black, isort, pytest, autoflake, and prettier installed.
+# Command for python checks is "pip install black isort autoflake pytest"
+# For prettier, if you have npm, run "npm install prettier".
+# Otherwise, in cmd, run: winget install OpenJS.NodeJS
+#       Then open a new cmd and run: npm install --save-dev prettier
+# VERY IMPORTANT WHEN INSTALLING PRETTIER RUN FROM NON-VS CODE TERMINAL OR CLOSE AND REOPEN VS CODE BETWEEN COMMANDS
+
 import os
 
 os.system("python -m isort . --profile black")
 os.system("python -m pytest")
+os.system("npx prettier --write .")
 os.system("python -m black .")
 print("If there is anything below this line it needs to be fixed for tests to pass")
 os.system(
