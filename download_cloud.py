1ba0152e2e24a29cd51bcab54619eeb78ec1631a
codingses <149913839+codingses@users.noreply.github.com>
2025-12-15 22:55:57 -0800

    Idk why this exists

--- download_cloud.py
+++ download_cloud.py
@@ -1,10 +1,10 @@
 import requests
 
-url = "https://storage.googleapis.com/epschedule-avatars/aec56023295543512d5395bf73e41d44cb5f8c99b38da0f4d4a1647879d3944a.jpg"
+url = "https://epschedule-avatars.storage.googleapis.com/ee8aa33ffbbf2f24b23d38b5c19a21246b2dae0a6b22592335a311c5f48dc00d.jpg"
 response = requests.get(url)
 if response.status_code == 200:
-    with open("cloud_cwest_icon.jpg", "wb") as f:
+    with open("cloud_ajosan_icon.jpg", "wb") as f:
         f.write(response.content)
-    print("Downloaded cloud image as cloud_cwest_icon.jpg")
+    print("Downloaded cloud image as cloud_ajosan_icon.jpg")
 else:
     print(f"Failed to download: {response.status_code}")
