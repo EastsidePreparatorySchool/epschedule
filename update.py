e2f6af3362dbc72bd07a825c7c75278f39ad26a4
Anmol Singh Josan <ajosan@eastsideprep.org>
2026-02-26 11:43:15 -0800

    Update update.py

--- update.py
+++ update.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     # Set up Google Application Credentials if not already set
     if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service_account.json"
-    
+
     # Parse command-line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("data", help="Which data update.")
@@ -26,26 +26,27 @@ if __name__ == "__main__":
     # Record start time for performance measurement
     start_time = time.time()
     print(
-        f"Updating {args.data}... dry run={args.dry_run} verbose={args.verbose} username={args.username}"
+        f"Updating {args.data}... dry run={args.dry_run} "
+        f"verbose={args.verbose} username={args.username}"
     )
-    
+
     # Determine which function to call based on the data type
-    callable = None
+    callable_func = None
     if args.data == "lunches":
-        callable = update_lunch.read_lunches
+        callable_func = update_lunch.read_lunches
     elif args.data == "photos":
-        callable = photos.crawl_photos
+        callable_func = photos.crawl_photos
     elif args.data == "schedules":
-        callable = schedules.crawl_schedules
+        callable_func = schedules.crawl_schedules
     else:
         print("Invalid data type.")
         exit(1)
 
     # Call the appropriate function with the provided arguments
     if args.data == "photos":
-        callable(args.dry_run, args.verbose, args.username)
+        callable_func(args.dry_run, args.verbose, args.username)
     else:
-        callable(args.dry_run, args.verbose)
-    
+        callable_func(args.dry_run, args.verbose)
+
     # Print the time taken for the operation
     print("Operation took {:.2f} seconds".format(time.time() - start_time))
