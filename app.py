f5abacf669e605063e61ee4bf2a5b353dabd06a7
codingses <149913839+codingses@users.noreply.github.com>
2026-05-06 16:37:52 -0700

    stuff for cam (#837)

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ VERSION = "1.32.55"  # Massive UI update/complete backend rework is the first nu
 
 import copy
 import datetime
+import hashlib
 import json
 import logging
 import os
 import re
+import urllib.request
 from queue import Empty, Queue
 
 import google.oauth2.id_token
@@ -398,12 +400,23 @@ def get_class_schedule(user_class, term_id):
     return result
 
 
+@app.route("/lunches/")
+def handle_lunches():
+    if "username" not in session:
+        abort(403)
+    return json.dumps(
+        get_lunches_since_date(datetime.date.today() - datetime.timedelta(28))
+    )
+
+
 # TODO rename this to /user since it's for students and teachers
 @app.route("/student/<target_user>")
-def handle_user(target_user):
+def handle_user(target_user, jsd=False):
     if "username" not in session:
         abort(403)
 
+    if target_user[0] == "[" and target_user[-1] == "]":
+        return json.dumps([handle_user(u, True) for u in target_user[1:-1].split(",")])
     user_schedule = get_schedule(session["username"])
     target_schedule = get_schedule(target_user)
     if user_schedule is None or target_schedule is None:
@@ -430,10 +443,16 @@ def handle_user(target_user):
     target_schedule["email"] = username_to_email(target_user)
 
     if priv_settings["share_photo"]:
-        target_schedule["photo_url"] = gen_photo_url(target_user, False)
+        target_schedule["photo_url"] = gen_photo_url(target_user, jsd)
+        if jsd:
+            h = hashlib.sha256()
+            with urllib.request.urlopen(target_schedule["photo_url"]) as r:
+                for chunk in iter(lambda: r.read(65536), b""):
+                    h.update(chunk)
+            target_schedule["photo_hash"] = h.hexdigest()
     else:
         target_schedule["photo_url"] = "/static/images/placeholder.png"
-    return json.dumps(target_schedule)
+    return json.dumps(target_schedule) if not jsd else target_schedule
 
 
 def sanitize_schedule(orig_schedule, user_schedule):
